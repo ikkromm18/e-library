d278a684983edaab0c96f5a55d1c5e267d2def5e
ikkromm18 <muhammadikkrom749@gmail.com>
2026-08-06 04:49:05 +0700

    fix: bind seeded buku to explicit kategori/rak

--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -3,14 +3,22 @@
 namespace Database\Seeders;
 
 use App\Models\Buku;
+use App\Models\Kategori;
+use App\Models\Rak;
 use Illuminate\Database\Seeder;
 
 class BookSeeder extends Seeder
 {
     public function run(): void
     {
+        $kategoriIds = Kategori::pluck('id')->all();
+        $rakIds = Rak::pluck('id')->all();
+
         foreach (range(1, 40) as $iteration) {
-            Buku::factory()->create();
+            Buku::factory()->create([
+                'kategori_id' => $kategoriIds[array_rand($kategoriIds)],
+                'rak_id' => $rakIds[array_rand($rakIds)],
+            ]);
         }
     }
 }
